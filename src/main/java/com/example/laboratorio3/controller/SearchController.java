77798527e246e10e86a01ffd9938991d69d37a30
ruthnadiezhda <a20150875@pucp.pe>
2020-05-04 21:00:13 -0500

    Departamentos Mayor Sueldo

--- src/main/java/com/example/laboratorio3/controller/SearchController.java
+++ src/main/java/com/example/laboratorio3/controller/SearchController.java
@@ -1,10 +1,12 @@
 package com.example.laboratorio3.controller;
 
+import com.example.laboratorio3.repository.DepartmentRepository;
 import com.example.laboratorio3.repository.EmployeesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 //COMPLETAR
@@ -17,6 +19,9 @@ public class SearchController {
     @Autowired
     EmployeesRepository employeesRepository;
 
+    @Autowired
+    DepartmentRepository departmentRepository;
+
     @GetMapping(value = {"","/"})
     public String indice(){
         return "Search/indice";
@@ -28,6 +33,13 @@ public class SearchController {
 
         return "Search/empleadosMayorSueldo";
     }
+
+    @GetMapping(value = {"/departamentoSalarioMaximo"})
+    public String listaProductos(Model model, @PathVariable ("max_salary") Integer max_salary) {
+        model.addAttribute("listaDepartamentos", departmentRepository.findAll());
+        model.addAttribute("listaEmpleados", employeesRepository.encontrarPorSueldo(max_salary));
+
+        return "Search/departamentoMaximoSalario";
 
 
 //COMPLETAR
--- src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
+++ src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
@@ -3,6 +3,7 @@ package com.example.laboratorio3.repository;
 
 import com.example.laboratorio3.dto.EmployeesJobsDto;
 import com.example.laboratorio3.entity.Employees;
+import com.example.laboratorio3.entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -20,6 +21,10 @@ public interface EmployeesRepository extends JpaRepository<Employees,Integer> {
             "WHERE (salary>15000)", nativeQuery= true)
     List<EmployeesJobsDto> obtenerEmpleadosSalarioMayor();
 
+
+
+    @Query()
+    public List <Employees> encontrarPorSueldo(Job job);
 
 
 
