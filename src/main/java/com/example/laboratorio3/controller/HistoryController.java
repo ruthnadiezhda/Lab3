ba7846b41c510a79219f54d714bd097a0c9c0488
JZambrano <jbzambrano@pucp.pe>
2020-05-04 19:47:12 -0500

    Prueba

--- src/main/java/com/example/laboratorio3/controller/HistoryController.java
+++ src/main/java/com/example/laboratorio3/controller/HistoryController.java
@@ -1,12 +1,30 @@
 package com.example.laboratorio3.controller;
 
 
+import com.example.laboratorio3.repository.EmployeesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 //COMPLETAR
 @Controller
+@RequestMapping(value="/history")
 public class HistoryController {
-//COMPLETAR
+
+    @Autowired
+    EmployeesRepository employeesRepository;
+
+    @GetMapping(value = {""})
+    public String listaEmpleados(Model model) {
+
+        model.addAttribute("listaProductos", employeesRepository.findAll());
+
+        return "product/list";
+    }
 
 
 }
--- src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
+++ src/main/java/com/example/laboratorio3/repository/EmployeesRepository.java
@@ -1,11 +1,13 @@
 package com.example.laboratorio3.repository;
 
 
+import com.example.laboratorio3.entity.Employees;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface EmployeesRepository {
+public interface EmployeesRepository extends JpaRepository<Employees,Integer> {
 
 //COMPLETAR
 
